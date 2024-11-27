76a956fe341b14b9128d7a332f53cb3394f8ea00
Osdark <odvelasquez@unac.edu.co>
2024-11-27 13:19:21 +0100

    ADDED security on regions, honors request

--- src/main/java/com/clubes/especialidades/api/controller/RegionControllerImpl.java
+++ src/main/java/com/clubes/especialidades/api/controller/RegionControllerImpl.java
@@ -5,6 +5,7 @@ import com.clubes.especialidades.api.service.RegionService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +19,7 @@ public class RegionControllerImpl implements RegionController {
 	private RegionService service;
 
 	@Override
+	@PreAuthorize("hasAuthority('ADMIN')")
 	public ResponseEntity<Region> createRegion(Region region, BindingResult bindingResult) {
 		if (bindingResult.hasErrors()) {
 			throw new IllegalArgumentException("Validation failed for request body: " + bindingResult.getFieldError());
@@ -26,6 +28,7 @@ public class RegionControllerImpl implements RegionController {
 	}
 
 	@Override
+	@PreAuthorize("hasAuthority('ADMIN')")
 	public ResponseEntity<Boolean> updateRegion(UUID id, Region region, BindingResult bindingResult) {
 		if (bindingResult.hasErrors()) {
 			throw new IllegalArgumentException("Validation failed for request body: " + bindingResult.getFieldError());
@@ -34,26 +37,31 @@ public class RegionControllerImpl implements RegionController {
 	}
 
 	@Override
+	@PreAuthorize("hasAuthority('ADMIN')")
 	public ResponseEntity<Boolean> deleteRegion(UUID id) {
 		return ResponseEntity.ok(service.deleteRegion(id));
 	}
 
 	@Override
+	@PreAuthorize("hasAuthority('ADMIN') or hasAuthority('USER')")
 	public ResponseEntity<List<Region>> getRegions() {
 		return ResponseEntity.ok(service.getRegions());
 	}
 
 	@Override
+	@PreAuthorize("hasAuthority('ADMIN') or hasAuthority('USER')")
 	public ResponseEntity<Region> getRegionById(UUID id) {
 		return ResponseEntity.ok(service.getRegion(id));
 	}
 
 	@Override
+	@PreAuthorize("hasAuthority('ADMIN') or hasAuthority('USER')")
 	public ResponseEntity<Region> getRegionByName(String name) {
 		return ResponseEntity.ok(service.getRegionByName(name));
 	}
 
 	@Override
+	@PreAuthorize("hasAuthority('ADMIN') or hasAuthority('USER')")
 	public ResponseEntity<Region> getRegionByAbbreviation(String abbreviation) {
 		return ResponseEntity.ok(service.getRegionByAbbreviation(abbreviation));
 	}
--- src/main/java/com/clubes/especialidades/api/controller/SpecialityControllerImpl.java
+++ src/main/java/com/clubes/especialidades/api/controller/SpecialityControllerImpl.java
@@ -5,6 +5,7 @@ import com.clubes.especialidades.api.service.SpecialityService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +19,7 @@ public class SpecialityControllerImpl implements SpecialityController {
 	private SpecialityService service;
 
 	@Override
+	@PreAuthorize("hasAuthority('ADMIN')")
 	public ResponseEntity<Speciality> createSpeciality(Speciality speciality, BindingResult bindingResult) {
 		if (bindingResult.hasErrors()) {
 			throw new IllegalArgumentException("Validation failed for request body: " + bindingResult.getFieldError());
@@ -26,6 +28,7 @@ public class SpecialityControllerImpl implements SpecialityController {
 	}
 
 	@Override
+	@PreAuthorize("hasAuthority('ADMIN')")
 	public ResponseEntity<Boolean> updateSpeciality(UUID id, Speciality speciality, BindingResult bindingResult) {
 		if (bindingResult.hasErrors()) {
 			throw new IllegalArgumentException("Validation failed for request body: " + bindingResult.getFieldError());
@@ -34,31 +37,37 @@ public class SpecialityControllerImpl implements SpecialityController {
 	}
 
 	@Override
+	@PreAuthorize("hasAuthority('ADMIN')")
 	public ResponseEntity<Boolean> deleteSpeciality(UUID id) {
 		return ResponseEntity.ok(service.deleteSpeciality(id));
 	}
 
 	@Override
+	@PreAuthorize("hasAuthority('ADMIN') or hasAuthority('USER')")
 	public ResponseEntity<List<Speciality>> getSpecialities() {
 		return ResponseEntity.ok(service.getSpecialities());
 	}
 
 	@Override
+	@PreAuthorize("hasAuthority('ADMIN') or hasAuthority('USER')")
 	public ResponseEntity<Speciality> getSpecialityById(UUID id) {
 		return ResponseEntity.ok(service.getSpecialityById(id));
 	}
 
 	@Override
+	@PreAuthorize("hasAuthority('ADMIN') or hasAuthority('USER')")
 	public ResponseEntity<Speciality> getSpecialityByName(String name) {
 		return ResponseEntity.ok(service.getSpecialityByName(name));
 	}
 
 	@Override
+	@PreAuthorize("hasAuthority('ADMIN') or hasAuthority('USER')")
 	public ResponseEntity<List<Speciality>> getSpecialitiesByArea(String area) {
 		return ResponseEntity.ok(service.getSpecialitiesByArea(area));
 	}
 
 	@Override
+	@PreAuthorize("hasAuthority('ADMIN') or hasAuthority('USER')")
 	public ResponseEntity<List<Speciality>> getSpecialitiesByRegion(String region) {
 		return ResponseEntity.ok(service.getSpecialitiesByRegion(region));
 	}

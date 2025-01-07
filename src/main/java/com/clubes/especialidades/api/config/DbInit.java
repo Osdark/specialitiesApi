45983ecd7ea2765f9ac888bdb39cb7677fef66b1
Osdark <odvelasquez@unac.edu.co>
2025-01-07 10:09:44 +0100

    clean import

--- src/main/java/com/clubes/especialidades/api/config/DbInit.java
+++ src/main/java/com/clubes/especialidades/api/config/DbInit.java
@@ -13,7 +13,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
-import java.util.UUID;
 
 @Slf4j
 @Component
